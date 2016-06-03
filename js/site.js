901bf1fb5a7d43663f1cd7d93e59173d4dc92d58
smallreflection <gwarner@justin-gammon-pro.worthwhilenetwork.net>
2016-06-03 14:09:58 -0400

    fixed waypoints

--- js/site.js
+++ js/site.js
@@ -1,13 +1,20 @@
-function onScrollAnimations() {
-  $('.wp-1').waypoint(function() {
-    $('.wp-1').addClass('animated fadeInUp');
-  }, {
-    offset: '75%'
-  });
+(function ($) {
+  "use strict";
 
-  $('.wp-2').waypoint(function() {
-    $('.wp-2').addClass('animated fadeInRight');
-  }, {
-    offset: '75%'
-  });
-}
+    function onScrollAnimations() {
+    $('.wp-1').waypoint(function() {
+      $('.wp-1').addClass('animated fadeInUp');
+    }, {
+      offset: '75%'
+    });
+
+    $('.wp-2').waypoint(function() {
+      $('.wp-2').addClass('animated fadeInRight');
+    }, {
+      offset: '75%'
+    });
+  }
+
+    onScrollAnimations();
+
+})(jQuery)
